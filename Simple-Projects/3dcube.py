efa08013e1966965c29f36eb9758042dda6a581a
timnirmal <timnirmal@gmail.com>
2022-02-02 22:39:26 +0530

    Cube Done Need to Rotate

--- Simple-Projects/3dcube.py
+++ Simple-Projects/3dcube.py
@@ -1,13 +1,35 @@
-import pygame
 import numpy as np
-import math
+import pygame
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 WIDTH, HEIGHT = (800, 600)
 
 pygame.display.set_caption("3D Projection of Cube")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Cube points
+points = [np.matrix([-1, -1, 1]), np.matrix([1, -1, 1]), np.matrix([1, 1, 1]), np.matrix([-1, 1, 1]),
+          np.matrix([-1, -1, -1]), np.matrix([1, -1, -1]), np.matrix([1, 1, -1]), np.matrix([-1, 1, -1])]
+
+# points.append(np.matrix([-1, -1, 1]))
+# points.append(np.matrix([1, -1, 1]))
+# points.append(np.matrix([1, 1, 1]))
+# points.append(np.matrix([-1, 1, 1]))
+# points.append(np.matrix([-1, -1, -1]))
+# points.append(np.matrix([1, -1, -1]))
+# points.append(np.matrix([1, 1, -1]))
+# points.append(np.matrix([-1, 1, -1]))
+
+# Now we need multiply this points by the "Projection Matrix"
+Projection_Matrix = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 0]])
+
+# Now in drawing each point is dot product with Projection Matrix
+
+scale = 100
+circle_pos = [WIDTH / 2, HEIGHT / 2]
+point_size = 7
 
 while True:
     for event in pygame.event.get():
@@ -21,6 +43,15 @@ while True:
 
     # Update Here
 
+
     screen.fill(WHITE)
 
     # Draw Stuff Here
+    # draw points
+    for point in points:
+        projected_point = np.dot(Projection_Matrix, point.reshape(3, 1))
+        x = int(projected_point[0][0] * scale) + circle_pos[0]
+        y = int(projected_point[1][0] * scale) + circle_pos[1]
+        pygame.draw.circle(screen, BLACK, (x, y), point_size)
+
+    pygame.display.update()
